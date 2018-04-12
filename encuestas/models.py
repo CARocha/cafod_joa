0a8d6dbc15f8a4f2075859dc54d80e0ab045141d
crocha <crocha09.09@gmail.com.com>
2018-04-12 14:33:14 -0600

    fix labels

--- encuestas/models.py
+++ encuestas/models.py
@@ -64,13 +64,14 @@ CHOICE_MIEMBRO_FAMILIA = (
                 (8, 'Niñas 5 a 12 años '),
               )
 CHOICE_MEDIDA = (
-                (1, 'Quintal'),
+                (1, 'Quintal(100kg)'),
                 (2, 'Libras'),
                 (3, 'Docena'),
                 (4, 'Cien'),
                 (5, 'Cabeza'),
                 (6, 'Litro'),
                 (7, 'Unidad'),
+                (8, 'Kilo'),
                 )
 # FIN DE CHOICES ESTATICOS
 
@@ -401,7 +402,7 @@ class CultivosAnuales(models.Model):
     area_cosechada = models.FloatField('Area cosechada (Ha)')
     cantidad_cosechada = models.FloatField()
     consumo_familia = models.FloatField('Consumo de la familia')
-    consumo_animal = models.FloatField()
+    consumo_animal = models.FloatField('Semilla')
     venta = models.FloatField()
 
     class Meta:
@@ -428,7 +429,7 @@ class CultivosHuerto(models.Model):
     area_cosechada = models.FloatField('Area cosechada (Ha)')
     cantidad_cosechada = models.FloatField()
     consumo_familia = models.FloatField('Consumo de la familia')
-    consumo_animal = models.FloatField()
+    consumo_animal = models.FloatField('Semilla')
     venta = models.FloatField()
 
     class Meta:
--- encuestas/views.py
+++ encuestas/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def _queryset_filtrado(request):
+    params = {}
+
+    if request.session['fecha']:
+        params['year'] = request.session['fecha']
+
+    # if 'estacion' in request.session:
+    #     params['estacion'] = request.session['estacion']
+
+    if 'pais' in request.session:
+        params['entrevistado__pais'] = request.session['pais']
+
+    if 'departamento' in request.session:
+        params['entrevistado__departamento__in'] = request.session['departamento']
+
+    if 'organizacion' in request.session:
+        params['org_responsable__in'] = request.session['organizacion']
+
+    if 'municipio' in request.session:
+        params['entrevistado__municipio__in'] = request.session['municipio']
+
+    if 'comunidad' in request.session:
+        params['entrevistado__comunidad__in'] = request.session['comunidad']
+
+    #if request.session['sexo']:
+    #    params['entrevistado__sexo'] = request.session['sexo']
+
+    unvalid_keys = []
+    for key in params:
+        if not params[key]:
+            unvalid_keys.append(key)
+
+    for key in unvalid_keys:
+        del params[key]
+
+    return Encuesta.objects.filter(**params)
